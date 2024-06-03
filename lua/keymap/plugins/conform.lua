f4e729e5888584bb2d2961f7f80097eb43446519
TropinoneH <mspt5871@protonmail.com>
2024-06-03 21:34:21 +0800

    finish conform configure

--- lua/keymap/plugins/conform.lua
+++ lua/keymap/plugins/conform.lua
@@ -1,15 +1,17 @@
 local bind = require("keymap.bind")
 local map_callback = bind.map_callback
+local map_cr = bind.map_cr
 
 bind.nvim_load_mapping({
-    ["nv|<leader>lf"] = map_callback(function ()
+    ["nv|<leader>lf"] = map_callback(function()
             require("conform").format({
                 lsp_fallback = true,
                 async = false,
-                timeout_ms = 500
+                timeout_ms = 500,
             })
         end)
         :with_silent()
         :with_noremap()
-        :with_desc("conform: format")
+        :with_desc("conform: format"),
+    ["n|<leader>ln"] = map_cr("ConformInfo"):with_silent():with_noremap():with_desc("conform: show format info"),
 })
--- lua/plugins/formatter/conform.lua
+++ lua/plugins/formatter/conform.lua
@@ -2,7 +2,7 @@ return {
     "stevearc/conform.nvim",
     event = { "BufReadPre", "BufNewFile" },
     opts = {
-        format_on_saved = {
+        format_on_save = {
             lsp_fallback = true,
             async = false,
             timeout_ms = 500,
