28911162129de37dfecd43e6ec69dfe187ccfed5
TropinoneH <mspt5871@protonmail.com>
2024-06-03 20:07:13 +0800

    add trouble keymap

--- lua/keymap/plugins/todo.lua
+++ lua/keymap/plugins/todo.lua
@@ -1,6 +1,9 @@
 local bind = require("keymap.bind")
 local map_cmd = bind.map_cmd
+local map_cr = bind.map_cr
 
 bind.nvim_load_mapping({
-    ["n|fT"] = map_cmd("<Cmd>TodoTelescope<CR>"):with_silent():with_noremap():with_desc("find: todos"),
+    ["n|<leader>fT"] = map_cmd("<Cmd>TodoTelescope<CR>"):with_silent():with_noremap():with_desc("find: todos"),
+    ["n|<leader>xt"] = map_cr("TodoTrouble"):with_noremap():with_silent():with_desc("todo: show trouble todo list"),
+    ["n|<leader>xT"] = map_cr("TodoTrouble keywords=TODO,FIX,FIXME"):with_noremap():with_silent():with_desc("todo: show todo/fixme/fix"),
 })
--- lua/keymap/plugins/trouble.lua
+++ lua/keymap/plugins/trouble.lua
@@ -2,22 +2,20 @@ local bind = require("keymap.bind")
 local map_cr = bind.map_cr
 
 bind.nvim_load_mapping({
-    ["n|<leader>lt"] = map_cr("TroubleToggle"):with_noremap():with_silent():with_desc("lsp: Toggle trouble list"),
-    ["n|<leader>ll"] = map_cr("TroubleToggle lsp_references")
-        :with_noremap()
-        :with_silent()
-        :with_desc("lsp: Show lsp references"),
-    ["n|<leader>lD"] = map_cr("TroubleToggle document_diagnostics")
-        :with_noremap()
-        :with_silent()
-        :with_desc("lsp: Show document diagnostics"),
-    ["n|<leader>lw"] = map_cr("TroubleToggle workspace_diagnostics")
+    ["n|<leader>xX"] = map_cr("Trouble diagnostics toggle")
         :with_noremap()
         :with_silent()
         :with_desc("lsp: Show workspace diagnostics"),
-    ["n|<leader>lq"] = map_cr("TroubleToggle quickfix")
+    ["n|<leader>xx"] = map_cr("Trouble diagnostics toggle filter.buf=0")
+        :with_noremap()
+        :with_silent()
+        :with_desc("lsp: Show diagnostics in current buf"),
+    ["n|<leader>xl"] = map_cr("Trouble loclist toggle")
+        :with_noremap()
+        :with_silent()
+        :with_desc("lsp: Show trouble location list"),
+    ["n|<leader>xq"] = map_cr("Trouble quickfix toggle")
         :with_noremap()
         :with_silent()
         :with_desc("lsp: Show quickfix list"),
-    ["n|<leader>lL"] = map_cr("TroubleToggle loclist"):with_noremap():with_silent():with_desc("lsp: Show loclist"),
 })
