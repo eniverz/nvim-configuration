8c8faac613138a915e0326103874091f3f60d61e
TropinoneH <mspt5871@protonmail.com>
2024-06-04 23:25:13 +0800

    update for fit current version

--- lua/plugins/lsp/trouble.lua
+++ lua/plugins/lsp/trouble.lua
@@ -12,7 +12,6 @@ return {
             position = "bottom", -- position of the list can be: bottom, top, left, right
             height = 10, -- height of the trouble list when position is top or bottom
             width = 50, -- width of the list when position is left or right
-            icons = true, -- use devicons for filenames
             mode = "document_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
             fold_open = icons.ui.ArrowOpen, -- icon used for open folds
             fold_closed = icons.ui.ArrowClosed, -- icon used for closed folds
