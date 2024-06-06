e768b538f6fc4f78ae3cdfe9685cf04debb2777a
TropinoneH <mspt5871@protonmail.com>
2024-06-06 09:48:07 +0800

    change mason to lspconfig's dependencies

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -13,6 +13,58 @@ return {
                 lazy = true,
                 config = true,
             },
+            {
+                "williamboman/mason.nvim",
+                cmd = {
+                    "Mason",
+                    "MasonInstall",
+                    "MasonUninstall",
+                    "MasonUninstallAll",
+                    "MasonLog",
+                },
+                opts = {
+                    ui = {
+                        icons = {
+                            package_installed = "✓",
+                            package_uninstalled = "✗",
+                            package_pending = "⟳",
+                        },
+                    },
+                    keymaps = {
+                        toggle_server_expand = "<CR>",
+                        install_server = "i",
+                        update_server = "u",
+                        check_server_version = "c",
+                        update_all_servers = "U",
+                        check_outdated_servers = "C",
+                        uninstall_server = "X",
+                        cancel_installation = "<C-c>",
+                    },
+                },
+                build = ":MasonUpdate",
+                config = require("plugins.config.mason"),
+            },
+            {
+                "williamboman/mason-lspconfig.nvim",
+                dependencies = { "williamboman/mason.nvim" },
+                lazy = true,
+                config = require("plugins.config.mason-lspconfig"),
+            },
+            {
+                "WhoIsSethDaniel/mason-tool-installer.nvim",
+                -- lazy = true,
+                dependencies = { "williamboman/mason.nvim" },
+                opts = {
+                    integrations = {
+                        ["mason-lspconfig"] = false,
+                        ["mason-null-ls"] = false,
+                        ["mason-nvim-dap"] = false,
+                    },
+                },
+                config = function(_, opts)
+                    require("mason-tool-installer").setup(opts)
+                end,
+            },
         },
         config = function(_, opts)
             require("lspconfig.ui.windows").default_options.border = "rounded"
@@ -88,7 +140,7 @@ return {
         optional = true,
         ---@type CatppuccinOptions
         opts = {
-            integrations = { lsp_saga = true, lsp_trouble = true },
+            integrations = { lsp_saga = true, lsp_trouble = true, mason = true },
         },
     },
 }
