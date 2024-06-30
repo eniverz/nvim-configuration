7f19d9d3522301f27acf2be99d1f4ded7736cc5f
TropinoneH <mspt5871@protonmail.com>
2024-06-30 13:38:43 +0800

    remove deprecated

--- lua/plugins/config/mason-lspconfig.lua
+++ lua/plugins/config/mason-lspconfig.lua
@@ -12,7 +12,7 @@ return function(_, opts)
         signs = true,
         underline = true,
         virtual_text = diagnostics_virtual_text and {
-            severity_limit = diagnostics_level,
+            min = diagnostics_level,
         } or false,
         -- set update_in_insert to false bacause it was enabled by lspsaga
         update_in_insert = false,
--- lua/plugins/editor/gitsigns.lua
+++ lua/plugins/editor/gitsigns.lua
@@ -8,36 +8,11 @@ return {
             local mapping = require("keymap.plugins.gitsigns")
             return {
                 signs = {
-                    add = {
-                        hl = "GitSignsAdd",
-                        text = "│",
-                        numhl = "GitSignsAddNr",
-                        linehl = "GitSignsAddLn",
-                    },
-                    change = {
-                        hl = "GitSignsChange",
-                        text = "│",
-                        numhl = "GitSignsChangeNr",
-                        linehl = "GitSignsChangeLn",
-                    },
-                    delete = {
-                        hl = "GitSignsDelete",
-                        text = "_",
-                        numhl = "GitSignsDeleteNr",
-                        linehl = "GitSignsDeleteLn",
-                    },
-                    topdelete = {
-                        hl = "GitSignsDelete",
-                        text = "‾",
-                        numhl = "GitSignsDeleteNr",
-                        linehl = "GitSignsDeleteLn",
-                    },
-                    changedelete = {
-                        hl = "GitSignsChange",
-                        text = "~",
-                        numhl = "GitSignsChangeNr",
-                        linehl = "GitSignsChangeLn",
-                    },
+                    add = { text = "│" },
+                    change = { text = "│" },
+                    delete = { text = "_" },
+                    topdelete = { text = "‾" },
+                    changedelete = { text = "~" },
                 },
                 on_attach = mapping,
                 watch_gitdir = { interval = 1000, follow_files = true },
