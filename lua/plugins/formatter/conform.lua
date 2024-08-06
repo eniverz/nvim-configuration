3c602e86a380614391c6c7b91b1179219de13d39
TropinoneH <mspt5871@protonmail.com>
2024-08-06 17:47:06 +0800

    conform: disable format on save

--- lua/plugins/formatter/conform.lua
+++ lua/plugins/formatter/conform.lua
@@ -2,11 +2,11 @@ return {
     "stevearc/conform.nvim",
     event = { "BufReadPre", "BufNewFile" },
     opts = {
-        format_on_save = {
-            lsp_fallback = true,
-            async = false,
-            timeout_ms = 500,
-        },
+        --     format_on_save = {
+        --         lsp_fallback = true,
+        --         async = false,
+        --         timeout_ms = 500,
+        --     },
     },
     config = function(_, opts)
         require("conform").setup(opts)
