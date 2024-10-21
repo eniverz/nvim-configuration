a16a7c80fbd722da16e769bf6ba2bc9b7e5cdec3
TropinoneH <mspt5871@protonmail.com>
2024-10-21 21:18:12 +0800

    update and remove nvim-conda

--- lua/plugins/languages/python.lua
+++ lua/plugins/languages/python.lua
@@ -71,10 +71,4 @@ return {
             },
         },
     },
-    {
-        "kmontocam/nvim-conda",
-        lazy = true,
-        ft = "python",
-        dependencies = { "nvim-lua/plenary.nvim" },
-    },
 }
