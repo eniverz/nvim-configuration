f5195c26b316508f9609de416b7901a4a0e30b2f
TropinoneH <mspt5871@protonmail.com>
2024-11-15 16:08:51 +0800

    add language support for hyprland configure file

---
+++ lua/plugins/languages/hypr.lua
@@ -0,0 +1,21 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        optional = true,
+        opts = function(_, opts)
+            if opts.ensure_installed ~= "all" then
+                opts.ensure_installed = require("utils.core").list_insert_unique(opts.ensure_installed, { "hyprlang" })
+            end
+            vim.filetype.add({
+                pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
+            })
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        optional = true,
+        opts = function(_, opts)
+            opts.ensure_installed = require("utils.core").list_insert_unique(opts.ensure_installed, { "hypr" })
+        end,
+    },
+}
