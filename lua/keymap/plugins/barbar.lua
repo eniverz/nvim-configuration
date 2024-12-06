4c2d3cabc5b8f8972297df1e473d305015ba20f2
eniverz <mspt5871@protonmail.com>
2024-12-07 00:07:17 +0800

    barbar: bufferline and add keymap

---
+++ lua/keymap/plugins/barbar.lua
@@ -0,0 +1,25 @@
+local bind = require("keymap.bind")
+local map_cr = bind.map_cr
+
+bind.nvim_load_mapping({
+    ["n|<A-Left>"] = map_cr("BufferPrevious"):with_noremap():with_silent():with_nowait():with_desc("Buffer: previous"),
+    ["n|<A-Right>"] = map_cr("BufferNext"):with_noremap():with_silent():with_nowait():with_desc("Buffer: next"),
+    ["n|<A-j>"] = map_cr("BufferPrevious"):with_noremap():with_silent():with_nowait():with_desc("Buffer: previous"),
+    ["n|<A-k>"] = map_cr("BufferNext"):with_noremap():with_silent():with_nowait():with_desc("Buffer: next"),
+    ["n|<A-S-Left>"] = map_cr("BufferMovePrevious"):with_noremap():with_silent():with_nowait():with_desc("Buffer: moveprevious"),
+    ["n|<A-S-Right>"] = map_cr("BufferMoveNext"):with_noremap():with_silent():with_nowait():with_desc("Buffer: move next"),
+    ["n|<A-S-j>"] = map_cr("BufferMovePrevious"):with_noremap():with_silent():with_nowait():with_desc("Buffer: move previous"),
+    ["n|<A-S-k>"] = map_cr("BufferMoveNext"):with_noremap():with_silent():with_nowait():with_desc("Buffer: move next"),
+    ["n|<leader>bP"] = map_cr("BufferPrevious"):with_noremap():with_silent():with_nowait():with_desc("Buffer: previous"),
+    ["n|<leader>bN"] = map_cr("BufferNext"):with_noremap():with_silent():with_nowait():with_desc("Buffer: next"),
+    ["n|<leader>bG"] = map_cr("BufferLast"):with_noremap():with_silent():with_nowait():with_desc("Buffer: go last"),
+    ["n|<leader>bc"] = map_cr("BufferClose"):with_noremap():with_silent():with_nowait():with_desc("Buffer: close current"),
+    ["n|<leader>br"] = map_cr("BufferRestore"):with_noremap():with_silent():with_nowait():with_desc("Buffer: restore closed"),
+    ["n|<leader>bp"] = map_cr("BufferPin"):with_noremap():with_silent():with_nowait():with_desc("Buffer: pin/unpin current"),
+
+    ["n|<leader>bsi"] = map_cr("BufferOrderByBufferNumber"):with_noremap():with_silent():with_nowait():with_desc("Buffer: sort by buffer id"),
+    ["n|<leader>bsn"] = map_cr("BufferOrderByName"):with_noremap():with_silent():with_nowait():with_desc("Buffer: sort by name"),
+    ["n|<leader>bsd"] = map_cr("BufferOrderByDirectory"):with_noremap():with_silent():with_nowait():with_desc("Buffer: sort by directory"),
+    ["n|<leader>bsl"] = map_cr("BufferOrderByLanguage"):with_noremap():with_silent():with_nowait():with_desc("Buffer: sort by code language"),
+    ["n|<leader>bsw"] = map_cr("BufferOrderByWindowNumber"):with_noremap():with_silent():with_nowait():with_desc("Buffer: sort by window number"),
+})
---
+++ lua/plugins/editor/barbar.lua
@@ -0,0 +1,40 @@
+return {
+    {
+        "romgrk/barbar.nvim",
+        dependencies = {
+            "lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
+            "nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
+        },
+        init = function()
+            vim.g.barbar_auto_setup = false
+        end,
+        opts = function()
+            local icons = { diagnostics = require("config.icons").get("diagnostics"), git = require("config.icons").get("git") }
+            return {
+                icons = {
+                    buffer_index = true,
+                    diagnostics = {
+                        [vim.diagnostic.severity.ERROR] = { enabled = true, icon = icons.diagnostics.Error_alt },
+                        [vim.diagnostic.severity.WARN] = { enabled = true, icon = icons.diagnostics.Warning_alt },
+                        [vim.diagnostic.severity.INFO] = { enabled = true, icon = icons.diagnostics.Information_alt },
+                        [vim.diagnostic.severity.HINT] = { enabled = true, icon = icons.diagnostics.Hint_alt },
+                    },
+                    gitsigns = {
+                        added = { enabled = true, icon = icons.git.Add },
+                        changed = { enabled = true, icon = icons.git.Modified },
+                        deleted = { enabled = true, icon = icons.git.Remove },
+                    },
+                },
+            }
+        end,
+    },
+    {
+        "stevearc/resession.nvim",
+        optional = true,
+        opts = {
+            extensions = {
+                barbar = {},
+            },
+        },
+    },
+}
