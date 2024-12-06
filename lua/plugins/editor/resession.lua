83c88f96ed6df01ef027fc445521ef4ca64219ab
eniverz <mspt5871@protonmail.com>
2024-12-07 00:08:00 +0800

    resession: not auto load

--- lua/plugins/editor/resession.lua
+++ lua/plugins/editor/resession.lua
@@ -21,17 +21,6 @@ return {
                     resession.save_tab(vim.fn.getcwd(), { notify = true })
                 end,
             })
-
-            -- Automatically load sessions on startup by working directory
-            vim.api.nvim_create_autocmd("VimEnter", {
-                callback = function()
-                    -- Only load the session if nvim was started with no args
-                    if vim.fn.argc(-1) == 0 then
-                        resession.load(vim.fn.getcwd(), { silence_errors = true })
-                    end
-                end,
-                nested = true,
-            })
         end,
     },
     {
