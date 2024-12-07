6dab08bc285954f9be3e9f6dad679a06d78a5f76
eniverz <mspt5871@protonmail.com>
2024-12-08 05:15:43 +0800

    add blink.cmp

---
+++ lua/plugins/lsp/blink.lua
@@ -0,0 +1,139 @@
+-- completion plugin
+return {
+    {
+        "saghen/blink.cmp",
+        lazy = true,
+        event = { "InsertEnter", "LspAttach" },
+        dependencies = { "giuxtaposition/blink-cmp-copilot", "rafamadriz/friendly-snippets" },
+        -- build = "cargo build --release",
+        version = "v0.*",
+        opts = {
+            sources = {
+                providers = {
+                    copilot = {
+                        name = "copilot",
+                        module = "blink-cmp-copilot",
+                    },
+                },
+                completion = {
+                    enabled_providers = { "lsp", "path", "snippets", "buffer", "copilot" },
+                },
+            },
+            completion = {
+                accept = {
+                    auto_brackets = { enabled = true },
+                },
+                menu = {
+                    border = "rounded",
+                    draw = {
+                        columns = { { "item_idx" }, { "label", "label_description", gap = 1 }, { "kind_icon" } },
+                        components = {
+                            item_idx = {
+                                text = function(ctx)
+                                    return tostring(ctx.idx)
+                                end,
+                                highlight = "BlinkCmpItemIdx", -- optional, only if you want to change its color
+                            },
+                        },
+                    },
+                },
+                documentation = {
+                    auto_show = true,
+                    window = { border = "single" }
+                },
+                signature = {
+                    enabled = true,
+                },
+            },
+            keymap = {
+                ["<A-1>"] = { function(cmp) cmp.accept({ index = 1 }) end, },
+                ["<A-2>"] = { function(cmp) cmp.accept({ index = 2 }) end, },
+                ["<A-3>"] = { function(cmp) cmp.accept({ index = 3 }) end, },
+                ["<A-4>"] = { function(cmp) cmp.accept({ index = 4 }) end, },
+                ["<A-5>"] = { function(cmp) cmp.accept({ index = 5 }) end, },
+                ["<A-6>"] = { function(cmp) cmp.accept({ index = 6 }) end, },
+                ["<A-7>"] = { function(cmp) cmp.accept({ index = 7 }) end, },
+                ["<A-8>"] = { function(cmp) cmp.accept({ index = 8 }) end, },
+                ["<A-9>"] = { function(cmp) cmp.accept({ index = 9 }) end, },
+                ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+                ["<C-e>"] = { "hide", "fallback" },
+                ["<ESC>"] = { "cancel", "fallback" },
+                ["<CR>"] = { "select_and_accept", "fallback" },
+                ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+                ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+                ["<Tab>"] = { "snippet_forward", "fallback" },
+                ["<S-Tab>"] = { "snippet_backward", "fallback" },
+            },
+            appearance = {
+                use_nvim_cmp_as_default = true,
+                kind_icons = {
+                    Array = "",
+                    Boolean = "󰨙",
+                    Class = "",
+                    Codeium = "󰘦",
+                    Color = "󰏘",
+                    Control = "",
+                    Collapsed = "",
+                    Constant = "󰏿",
+                    Constructor = "",
+                    Copilot = " ",
+                    Enum = "󰦨",
+                    EnumMember = "",
+                    Event = "",
+                    Field = "󰜢",
+                    File = "",
+                    Folder = "",
+                    Function = "󰊕",
+                    Interface = "󱡠",
+                    Key = "",
+                    Keyword = "",
+                    Method = "󰊕",
+                    Module = "",
+                    Namespace = "󰦮",
+                    Null = "",
+                    Number = "󰎠",
+                    Object = "",
+                    Operator = "",
+                    Package = "",
+                    Property = "",
+                    Reference = "",
+                    Snippet = "",
+                    String = "",
+                    Struct = "󰅩",
+                    TabNine = "󰏚",
+                    Text = "󰉿",
+                    TypeParameter = "",
+                    Unit = "",
+                    Value = "",
+                    Variable = "",
+                },
+            },
+        },
+    },
+    {
+        "giuxtaposition/blink-cmp-copilot",
+        lazy = true,
+        dependencies = {
+            "zbirenbaum/copilot.lua",
+            cmd = "Copilot",
+            event = "InsertEnter",
+            opts = {
+                panel = {
+                    -- if true, it can interfere with completions in copilot-cmp
+                    enabled = false,
+                },
+                suggestion = {
+                    -- if true, it can interfere with completions in copilot-cmp
+                    enabled = false,
+                },
+                filetypes = {
+                    ["dap-repl"] = false,
+                    ["big_file_disabled_ft"] = false,
+                },
+            },
+            config = function(_, opts)
+                require("copilot").setup(opts)
+            end,
+        },
+    },
+}
