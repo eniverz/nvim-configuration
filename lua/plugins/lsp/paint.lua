68ebac445f1114287468b3daee391463992cb538
eniverz <mspt5871@protonmail.com>
2024-12-09 16:52:58 +0800

    add paint plugin

---
+++ lua/plugins/lsp/paint.lua
@@ -0,0 +1,27 @@
+-- highlight the comment with special token
+return {
+    "folke/paint.nvim",
+    lazy = true,
+    event = { "CursorHold", "CursorHoldI" },
+    opts = {
+        ---@type PaintHighlight[]
+        highlights = {
+            {
+                -- filter can be a table of buffer options that should match,
+                -- or a function called with buf as param that should return true.
+                -- The example below will paint @something in comments with Constant
+                filter = { filetype = "lua" },
+                pattern = "%s*%-%-%-%s*(@%w+)",
+                hl = "Constant",
+            },
+            {
+                filter = { filetype = "python" },
+                pattern = "%s*([_%w]+:)",
+                hl = "Constant",
+            },
+        },
+    },
+    config = function(_, opts)
+        require("paint").setup(opts)
+    end,
+}
