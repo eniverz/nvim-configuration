590fcd76ac91e8a0b5f0dd30b4c8b5a3e62e8038
eniverz <mspt5871@protonmail.com>
2024-12-10 17:02:08 +0800

    add optional tag for treesitter in language pack

--- lua/plugins/languages/markdown.lua
+++ lua/plugins/languages/markdown.lua
@@ -2,6 +2,7 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        optional = true,
         opts = function(_, opts)
             if opts ~= nil then
                 opts = require("utils.core").list_insert_unique(opts.ensure_installed, { "markdown", "markdown_inline" })
