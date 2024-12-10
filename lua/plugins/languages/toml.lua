7e46257ee5231d1d40e45035d1a39baa216095e5
eniverz <mspt5871@protonmail.com>
2024-12-10 20:16:44 +0800

    add toml language pack

---
+++ lua/plugins/languages/toml.lua
@@ -0,0 +1,27 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        optional = true,
+        opts = function(_, opts)
+            -- Ensure that opts.ensure_installed exists and is a table or string "all".
+            if opts.ensure_installed ~= "all" then
+                opts.ensure_installed = require("utils.core").list_insert_unique(opts.ensure_installed, { "toml" })
+            end
+        end,
+    },
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        optional = true,
+        opts = function(_, opts)
+            opts.ensure_installed = require("utils.core").list_insert_unique(opts.ensure_installed, { "taplo" })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        optional = true,
+        opts = function(_, opts)
+            opts.server = opts.server or {}
+            opts.server.taplo = {}
+        end,
+    }
+}
