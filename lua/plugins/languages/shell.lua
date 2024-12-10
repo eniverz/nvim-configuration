27f2af284cb990e3300caebcaad5c2c8efb641d8
eniverz <mspt5871@protonmail.com>
2024-12-10 20:05:54 +0800

    add shell language pack

---
+++ lua/plugins/languages/shell.lua
@@ -0,0 +1,67 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        optional = true,
+        opts = function(_, opts)
+            if opts.ensure_installed ~= "all" then
+                opts.ensure_installed = require("utils.core").list_insert_unique(opts.ensure_installed, { "bash" })
+            end
+        end,
+    },
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        optional = true,
+        opts = function(_, opts)
+            opts.ensure_installed =
+                require("utils.core").list_insert_unique(opts.ensure_installed, { "bash-language-server", "shellcheck", "shfmt", "bash-debug-adapter" })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        optional = true,
+        opts = function(_, opts)
+            opts.server = opts.server or {}
+            opts.server.bashls = {
+                settings = {
+                    bashIde = {
+                        globPattern = vim.env.GLOB_PATTERN or "*@(.sh|.inc|.bash|.command|.zsh)",
+                    },
+                },
+            }
+        end,
+    },
+    {
+        "stevearc/conform.nvim",
+        optional = true,
+        opts = {
+            formatters_by_ft = {
+                sh = { "shfmt" },
+                bash = { "shfmt" },
+                fish = { "shfmt" },
+            },
+            formatters = {
+                shfmt = {
+                    command = "shfmt",
+                    args = {
+                        "-i", -- indent with 4 spaces
+                        "4",
+                        "-sr", -- redirect operator will be followed by a space
+                        "-kp", -- Keep column alignment paddings
+                        "-ci", -- Switch cases will be indented
+                        "-filename",
+                        "$FILENAME",
+                    },
+                },
+            },
+        },
+    },
+    {
+        "mfussenegger/nvim-lint",
+        optional = true,
+        opts = {
+            linters_by_ft = {
+                sh = { "shellcheck" },
+            },
+        },
+    },
+}
