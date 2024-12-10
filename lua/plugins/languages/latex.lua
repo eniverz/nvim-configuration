cf641e5b79dc2dcd05fafea2452809ccbbcda316
eniverz <mspt5871@protonmail.com>
2024-12-10 22:04:10 +0800

    fix bugs in latex language pack

--- lua/plugins/languages/latex.lua
+++ lua/plugins/languages/latex.lua
@@ -18,6 +18,7 @@ return {
     {
         "neovim/nvim-lspconfig",
         opts = function(_, opts)
+            opts.servers = opts.servers or {}
             opts.servers.texlab = {}
         end,
     },
