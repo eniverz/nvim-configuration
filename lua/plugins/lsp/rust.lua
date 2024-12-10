827e8e467c223026573a3caef1eb52a90b1c9676
eniverz <mspt5871@protonmail.com>
2024-12-10 21:26:57 +0800

    fix bugs for rust crates plugin

--- lua/plugins/lsp/rust.lua
+++ lua/plugins/lsp/rust.lua
@@ -32,7 +32,87 @@ return {
                 hover = true,
             },
         },
-        config = require("plugins.config.crates"),
+        config = function(_, opts)
+            local icons = {
+                diagnostics = require("config.icons").get("diagnostics", true),
+                git = require("config.icons").get("git", true),
+                misc = require("config.icons").get("misc", true),
+                ui = require("config.icons").get("ui", true),
+                kind = require("config.icons").get("kind", true),
+            }
+
+            -- vim.api.nvim_create_autocmd("BufRead", {
+            --     group = vim.api.nvim_create_augroup("CmpSourceCargo", { clear = true }),
+            --     desc = "Load crates.nvim into Cargo buffers",
+            --     pattern = "Cargo.toml",
+            --     callback = function()
+            --         require("cmp").setup.buffer({ sources = { { name = "crates" } } })
+            --         require("crates")
+            --     end,
+            -- })
+            --
+            local new_opts = {
+                text = {
+                    loading = " " .. icons.misc.Watch .. "Loading",
+                    version = " " .. icons.ui.Check .. "%s",
+                    prerelease = " " .. icons.diagnostics.Warning_alt .. "%s",
+                    yanked = " " .. icons.diagnostics.Error .. "%s",
+                    nomatch = " " .. icons.diagnostics.Question .. "No match",
+                    upgrade = " " .. icons.diagnostics.Hint_alt .. "%s",
+                    error = " " .. icons.diagnostics.Error .. "Error fetching crate",
+                },
+                popup = {
+                    hide_on_select = true,
+                    border = "rounded",
+                    show_version_date = true,
+                    text = {
+                        title = icons.ui.Package .. "%s",
+                        description = "%s",
+                        created_label = icons.misc.Added .. "created" .. "        ",
+                        created = "%s",
+                        updated_label = icons.misc.ManUp .. "updated" .. "        ",
+                        updated = "%s",
+                        downloads_label = icons.ui.CloudDownload .. "downloads      ",
+                        downloads = "%s",
+                        homepage_label = icons.misc.Campass .. "homepage       ",
+                        homepage = "%s",
+                        repository_label = icons.git.Repo .. "repository     ",
+                        repository = "%s",
+                        documentation_label = icons.diagnostics.Information_alt .. "documentation  ",
+                        documentation = "%s",
+                        crates_io_label = icons.ui.Package .. "crates.io      ",
+                        crates_io = "%s",
+                        categories_label = icons.kind.Class .. "categories     ",
+                        keywords_label = icons.kind.Keyword .. "keywords       ",
+                        version = "  %s",
+                        prerelease = icons.diagnostics.Warning_alt .. "%s prerelease",
+                        yanked = icons.diagnostics.Error .. "%s yanked",
+                        version_date = "  %s",
+                        feature = "  %s",
+                        enabled = icons.ui.Play .. "%s",
+                        transitive = icons.ui.List .. "%s",
+                        normal_dependencies_title = icons.kind.Interface .. "Dependencies",
+                        build_dependencies_title = icons.misc.Gavel .. "Build dependencies",
+                        dev_dependencies_title = icons.misc.Glass .. "Dev dependencies",
+                        dependency = "  %s",
+                        optional = icons.ui.BigUnfilledCircle .. "%s",
+                        dependency_version = "  %s",
+                        loading = " " .. icons.misc.Watch,
+                    },
+                },
+                completion = {
+                    text = {
+                        prerelease = " " .. icons.diagnostics.Warning_alt .. "pre-release ",
+                        yanked = " " .. icons.diagnostics.Error_alt .. "yanked ",
+                    },
+                },
+            }
+            opts = vim.tbl_extend("force", opts, new_opts)
+            require("crates").setup(opts)
+
+            -- Set buffer-local keymaps
+            require("keymap.plugins.crates")
+        end,
     },
     {
         "mrcjkb/rustaceanvim",
@@ -96,19 +176,15 @@ return {
 
             ---@type RustaceanOpts
             ---@diagnostic disable: missing-fields
-            vim.g.rustaceanvim = vim.tbl_extend(
-                "keep",
-                vim.g.rustaceanvim or {},
-                {
-                    tools = {
-                        enable_clippy = false,
-                    },
-                    dap = {
-                        adapter = adapter,
-                    },
-                    server = server,
-                }
-            )
+            vim.g.rustaceanvim = vim.tbl_extend("keep", vim.g.rustaceanvim or {}, {
+                tools = {
+                    enable_clippy = false,
+                },
+                dap = {
+                    adapter = adapter,
+                },
+                server = server,
+            })
         end,
     },
 }
