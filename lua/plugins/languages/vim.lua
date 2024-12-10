e264cf48a76d5f74ee9b6641eb97cbdfaba90116
eniverz <mspt5871@protonmail.com>
2024-12-10 20:17:17 +0800

    add vim language pack

---
+++ lua/plugins/languages/vim.lua
@@ -0,0 +1,26 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        optional = true,
+        opts = function(_, opts)
+            if opts.ensure_installed ~= "all" then
+                opts.ensure_installed = require("utils.core").list_insert_unique(opts.ensure_installed, { "vim", "vimdoc" })
+            end
+        end,
+    },
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        optional = true,
+        opts = function(_, opts)
+            opts.ensure_installed = require("utils.core").list_insert_unique(opts.ensure_installed, { "vim-language-server" })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        optional = true,
+        opts = function(_, opts)
+            opts.server = opts.server or {}
+            opts.server.vimls = {}
+        end,
+    },
+}
