0d07b461592d900b2dacef847eed52c0504dff34
eniverz <mspt5871@protonmail.com>
2024-12-11 23:31:55 +0800

    update keymap description of lspsaga

--- lua/keymap/plugins/lspsaga.lua
+++ lua/keymap/plugins/lspsaga.lua
@@ -11,9 +11,10 @@ return function(bufnr)
         ["n|<leader>ld"] = map_cr("Lspsaga peek_definition"):with_silent():with_buffer(bufnr):with_desc("lsp: peek definition under cursor"),
         ["n|<leader>lD"] = map_cr("Lspsaga goto_definition"):with_silent():with_buffer(bufnr):with_desc("lsp: goto definition for cursor"),
         ["n|<leader>ls"] = map_cr("Lspsaga outline"):with_silent():with_buffer(bufnr):with_desc("lsp: show symbol outline"),
+        -- diagnostic
         ["n|<leader>xj"] = map_cr("Lspsaga diagnostic_jump_next"):with_silent():with_buffer(bufnr):with_desc("lspsaga: goto next diagnostic"),
         ["n|<leader>xk"] = map_cr("Lspsaga diagnostic_jump_prev"):with_silent():with_buffer(bufnr):with_desc("lspsaga: goto prev diagnostic"),
-        ["n|<leader>xx"] = map_cr("Lspsaga show_buf_diagnostics"):with_silent():with_buffer(bufnr):with_desc("lspsaga: goto prev diagnostic"),
-        ["n|<leader>xX"] = map_cr("Lspsaga show_workspace_diagnostics"):with_silent():with_buffer(bufnr):with_desc("lspsaga: goto prev diagnostic"),
+        ["n|<leader>xx"] = map_cr("Lspsaga show_buf_diagnostics"):with_silent():with_buffer(bufnr):with_desc("lspsaga: show diagnostics on current buffer"),
+        ["n|<leader>xX"] = map_cr("Lspsaga show_workspace_diagnostics"):with_silent():with_buffer(bufnr):with_desc("lspsaga: show diagnostics on workspace"),
     }
 end
