edce7401414fba0d73be28dd98c462e503dd5764
eniverz <mspt5871@protonmail.com>
2024-12-25 11:32:08 +0800

    add latex autopair rules

--- lua/plugins/lsp/autopairs.lua
+++ lua/plugins/lsp/autopairs.lua
@@ -7,7 +7,7 @@ return {
         -- ts_config = { java = false },
         fast_wrap = {
             map = "<A-e>",
-            chars = { "{", "[", "(", '"', "'" },
+            chars = { "{", "[", "(", '"', "'", "<" },
             pattern = ([[ [%'%"%)%>%]%)%}%,] ]]):gsub("%s+", ""),
             offset = 0,
             end_key = "$",
@@ -19,7 +19,29 @@ return {
     },
     config = function(_, opts)
         local npairs = require("nvim-autopairs")
+        local cond = require("nvim-autopairs.conds")
+        local Rule = require("nvim-autopairs.rule")
         npairs.setup(opts)
+        npairs.add_rules({
+            Rule("$", "$", { "tex", "latex" })
+                -- don't add a pair if the next character is %
+                :with_pair(cond.not_after_regex("%%"))
+                -- don't add a pair if  the previous character is xxx
+                :with_pair(cond.not_before_regex("xxx", 3))
+                -- don't move right when repeat character
+                :with_move(cond.none())
+                -- don't delete if the next character is xx
+                :with_del(cond.not_after_regex("xx"))
+                -- disable adding a newline when you press <cr>
+                :with_cr(cond.none()),
+        }, {
+            Rule("$$", "$$", "tex"):with_pair(function(opt)
+                if opt.line == "aa $$" then
+                    -- don't add pair on that line
+                    return false
+                end
+            end),
+        })
 
         if not require("config.settings").lsp.features.autopairs then
             npairs.disable()
