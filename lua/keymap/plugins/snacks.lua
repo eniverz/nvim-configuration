40a434c5c6304af7c3bb0bfe120a75d7a6a24bae
eniverz <mspt5871@protonmail.com>
2024-12-27 02:26:04 +0800

    move toggle maps to keymap file

--- lua/keymap/plugins/snacks.lua
+++ lua/keymap/plugins/snacks.lua
@@ -1,8 +1,6 @@
 local bind = require("keymap.bind")
 local map_callback = bind.map_callback
 
-local indent = true
-
 bind.nvim_load_mapping({
     -- notifier
     ["n|<leader>ud"] = map_callback(function()
@@ -58,18 +56,13 @@ bind.nvim_load_mapping({
         :with_nowait()
         :with_silent()
         :with_desc("Snacks: jump next word"),
-
-    -- indent
-    ["n|<leader>u|"] = map_callback(function()
-            if indent then
-                Snacks.indent.disable()
-            else
-                Snacks.indent.enable()
-            end
-            indent = not indent
-        end)
-        :with_noremap()
-        :with_nowait()
-        :with_silent()
-        :with_desc("Snacks: toggle indent"),
 })
+
+Snacks.toggle.diagnostics():map("<leader>uD")
+Snacks.toggle.line_number():map("<leader>ul")
+Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+Snacks.toggle.treesitter():map("<leader>ut")
+Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
+Snacks.toggle.indent():map("<leader>u|")
+Snacks.toggle.zen():map("<leader>uz")
--- lua/plugins/editor/snacks.lua
+++ lua/plugins/editor/snacks.lua
@@ -2,20 +2,6 @@
 return {
     "folke/snacks.nvim",
     priority = 1000,
-    init = function()
-        vim.api.nvim_create_autocmd("User", {
-            pattern = "VeryLazy",
-            callback = function()
-                -- Create some toggle mappings
-                Snacks.toggle.diagnostics():map("<leader>uD")
-                Snacks.toggle.line_number():map("<leader>ul")
-                Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
-                Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
-                Snacks.toggle.treesitter():map("<leader>ut")
-                Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
-            end,
-        })
-    end,
     opts = {
         bigfile = {
             enabled = true,
