cc5e2eafabfdaa0a882d4d37ee5258642ff87eaf
eniverz <mspt5871@protonmail.com>
2024-12-27 11:35:47 +0800

    add yazi plugin

---
+++ lua/plugins/editor/yazi.lua
@@ -0,0 +1,17 @@
+return {
+    "mikavilpas/yazi.nvim",
+    cmd = "Yazi",
+    opts = {},
+    keys = {
+        {
+            "<leader>ny",
+            "<cmd>Yazi<cr>",
+            desc = "Yazi: show current file",
+        },
+        {
+            "<leader>nY",
+            "<cmd>Yazi cwd<cr>",
+            desc = "Yazi: show current working directory",
+        },
+    },
+}
