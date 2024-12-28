3621d70eef4f2fcc59ea54e045b6fc79b0b598b4
eniverz <mspt5871@protonmail.com>
2024-12-28 18:57:18 +0800

    add diffview keymaps

--- lua/plugins/editor/diffview.lua
+++ lua/plugins/editor/diffview.lua
@@ -2,4 +2,18 @@
 return {
     "sindrets/diffview.nvim",
     cmd = { "DiffviewOpen", "DiffviewFileHistory" },
+    opts = function()
+        local actions = require("diffview.actions")
+
+        return {
+            keymaps = {
+                file_panel = {
+                    { "n", "<leader>do", actions.conflict_choose_all("ours"), { desc = "Choose the OURS version of a conflict for the whole file" } },
+                    { "n", "<leader>dt", actions.conflict_choose_all("theirs"), { desc = "Choose the THEIRS version of a conflict for the whole file" } },
+                    { "n", "<leader>db", actions.conflict_choose_all("base"), { desc = "Choose the BASE version of a conflict for the whole file" } },
+                    { "n", "<leader>da", actions.conflict_choose_all("all"), { desc = "Choose all the versions of a conflict for the whole file" } },
+                },
+            },
+        }
+    end,
 }
