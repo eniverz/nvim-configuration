b631d4d645f5944e72b5a7ad23dcbb3ce4a6f401
eniverz <mspt5871@protonmail.com>
2024-12-28 18:49:05 +0800

    avoid jk to escape terminal

--- lua/plugins/editor/better-escape.lua
+++ lua/plugins/editor/better-escape.lua
@@ -2,5 +2,12 @@
 return {
     "max397574/better-escape.nvim",
     event = { "CursorHold", "CursorHoldI", "InsertCharPre" },
-    opts = { timeout = 300 },
+    opts = {
+        timeout = 300,
+        default_mappings = false,
+        mappings = {
+            i = { j = { j = "<Esc>" } },
+            c = { j = { j = "<Esc>" } },
+        },
+    },
 }
