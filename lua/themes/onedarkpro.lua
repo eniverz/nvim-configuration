3a3ba8776b2e99091d1a92c56ef7eb8dfb15f73b
eniverz <mspt5871@protonmail.com>
2025-01-11 04:39:23 +0800

    fix bugs in onedark hls

--- lua/themes/onedarkpro.lua
+++ lua/themes/onedarkpro.lua
@@ -5,7 +5,7 @@ return {
     config = function(_, opts)
         local scheme = require("config.scheme")
         opts.highlights = vim.tbl_extend("force", opts.highlights or {}, scheme.highlight_overrides)
-        local colors = require("onedarkpro.theme").colors(scheme.colorscheme:find("onedark") and scheme.colorscheme or "onedark_dark")
+        local colors = require("onedarkpro.theme").colors(scheme.colorscheme:find("onedark") and scheme.colorscheme or "onedark_vivid")
         local lsp_kind_icons_color = {
             Default = colors.purple,
             Array = colors.yellow,
@@ -27,7 +27,7 @@ return {
             Method = colors.blue,
             Module = colors.orange,
             Namespace = colors.red,
-            Null = colors.grey,
+            Null = colors.gray,
             Number = colors.orange,
             Object = colors.red,
             Operator = colors.red,
@@ -37,7 +37,7 @@ return {
             Snippet = colors.red,
             String = colors.green,
             Struct = colors.purple,
-            Text = colors.grey,
+            Text = colors.gray,
             TypeParameter = colors.red,
             Unit = colors.green,
             Value = colors.orange,
@@ -46,6 +46,57 @@ return {
         for kind, color in pairs(lsp_kind_icons_color) do
             opts.highlights["LspKind" .. kind] = { fg = color }
         end
+        local blink_cmp_hls = {
+            BlinkCmpMenuBorder = { fg = colors.gray },
+            BlinkCmpMenuSelection = { fg = colors.none, bg = colors.comment },
+            BlinkCmpLabelDeprecated = { fg = colors.comment },
+            BlinkCmpLabelMatch = { fg = colors.cyan, bold = true },
+            BlinkCmpGhostText = { fg = colors.comment },
+            BlinkCmpDocBorder = { fg = colors.gray },
+            BlinkCmpSignatureHelpBorder = { fg = colors.gray },
+            BlinkCmpKindArray = { fg = colors.yellow },
+            BlinkCmpKindBoolean = { fg = colors.orange },
+            BlinkCmpKindClass = { fg = colors.yellow },
+            BlinkCmpKindColor = { fg = colors.red },
+            BlinkCmpKindConstant = { fg = colors.orange },
+            BlinkCmpKindConstructor = { fg = colors.blue },
+            BlinkCmpKindEnum = { fg = colors.purple },
+            BlinkCmpKindEnumMember = { fg = colors.yellow },
+            BlinkCmpKindEvent = { fg = colors.yellow },
+            BlinkCmpKindField = { fg = colors.purple },
+            BlinkCmpKindFile = { fg = colors.blue },
+            BlinkCmpKindFolder = { fg = colors.orange },
+            BlinkCmpKindFunction = { fg = colors.blue },
+            BlinkCmpKindInterface = { fg = colors.green },
+            BlinkCmpKindKey = { fg = colors.cyan },
+            BlinkCmpKindKeyword = { fg = colors.cyan },
+            BlinkCmpKindMethod = { fg = colors.blue },
+            BlinkCmpKindModule = { fg = colors.orange },
+            BlinkCmpKindNamespace = { fg = colors.red },
+            BlinkCmpKindNull = { fg = colors.gray },
+            BlinkCmpKindNumber = { fg = colors.orange },
+            BlinkCmpKindObject = { fg = colors.red },
+            BlinkCmpKindOperator = { fg = colors.red },
+            BlinkCmpKindPackage = { fg = colors.yellow },
+            BlinkCmpKindProperty = { fg = colors.cyan },
+            BlinkCmpKindReference = { fg = colors.orange },
+            BlinkCmpKindSnippet = { fg = colors.green },
+            BlinkCmpKindString = { fg = colors.green },
+            BlinkCmpKindStruct = { fg = colors.purple },
+            BlinkCmpKindText = { fg = colors.green },
+            BlinkCmpKindTypeParameter = { fg = colors.red },
+            BlinkCmpKindUnit = { fg = colors.green },
+            BlinkCmpKindValue = { fg = colors.orange },
+            BlinkCmpKindVariable = { fg = colors.purple },
+            BlinkCmpKindCodeium = { fg = colors.green },
+            BlinkCmpKindCopilot = { fg = colors.cyan },
+            BlinkCmpKind = { fg = colors.blue },
+        }
+        for group, hl in pairs(blink_cmp_hls) do
+            opts.highlights[group] = hl
+        end
+        local helpers = require("onedarkpro.helpers")
+        opts.highlights["CursorColumn"] = { bg = helpers.lighten(colors.bg, 10) }
         opts.options = vim.tbl_extend("force", opts.options or {}, {
             cursorline = true,
             transparency = scheme.transparent_background,
