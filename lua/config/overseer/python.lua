787d285baee089ce94607802dc5c3157a7b74996
eniverz <mspt5871@protonmail.com>
2025-03-29 00:47:18 +0800

    fix bus for overseer python, when conda env not found

--- lua/config/overseer/python.lua
+++ lua/config/overseer/python.lua
@@ -8,24 +8,25 @@ end
 return {
     name = "python",
     priority = 60,
-    params = {
-        cmd = { type = "string", default = vim.fn.getenv("CONDA_PREFIX") .. "/bin/python", order = 1 },
-        target = { type = "string", default = relpath(vim.fn.expand("%:p"), vim.fn.getcwd(0, 0) .. "/"), order = 2 },
-        args = { type = "list", delimiter = " ", optional = true, order = 3 },
-        cwd = { optional = true, default = vim.fn.getcwd(0, 0), order = 4 },
-    },
+    params = function()
+        local default_cmd = os.getenv("CONDA_PREFIX") .. "/bin/python"
+        if not vim.fn.executable(default_cmd) then
+            default_cmd = vim.fn.getenv("VIRTUAL_ENV") .. "/bin/python"
+        end
+        if not vim.fn.executable(default_cmd) then
+            default_cmd = vim.fn.exepath("python")
+        end
+
+        return {
+            cmd = { type = "string", default = default_cmd, order = 1 },
+            target = { type = "string", default = relpath(vim.fn.expand("%:p"), vim.fn.getcwd(0, 0) .. "/"), order = 2 },
+            args = { type = "list", delimiter = " ", optional = true, order = 3 },
+            cwd = { optional = true, default = vim.fn.getcwd(0, 0), order = 4 },
+        }
+    end,
     condition = { filetype = { "python" } },
     builder = function(params)
         local cmd = params.cmd
-        if not vim.fn.executable(cmd) then
-            cmd = vim.fn.getenv("VIRTUAL_ENV") .. "/bin/python"
-        end
-        if not vim.fn.executable(cmd) then
-            cmd = vim.fn.exepath("python")
-        end
-        if not vim.fn.executable(cmd) then
-            return nil
-        end
         return {
             cmd = { cmd, params.target },
             args = params.args,
