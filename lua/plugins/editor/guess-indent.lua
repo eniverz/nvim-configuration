c747dfdf9d99f5af0ea0e988b298d50a79d835dc
eniverz <mspt5871@protonmail.com>
2025-04-25 01:40:35 +0800

    update and add guess-indent

---
+++ lua/plugins/editor/guess-indent.lua
@@ -0,0 +1,31 @@
+return {
+    "nmac427/guess-indent.nvim",
+    lazy = true,
+    event = { "FileType", "BufReadPre" },
+    opts = {
+        auto_cmd = true, -- Set to false to disable automatic execution
+        override_editorconfig = false, -- Set to true to override settings set by .editorconfig
+        filetype_exclude = { -- A list of filetypes for which the auto command gets disabled
+            "netrw",
+            "tutor",
+            "snacks_dashboard",
+            "big_file_disabled_ft",
+            "nofile",
+        },
+        buftype_exclude = { -- A list of buffer types for which the auto command gets disabled
+            "help",
+            "nofile",
+            "terminal",
+            "prompt",
+        },
+        on_tab_options = { -- A table of vim options when tabs are detected
+            ["expandtab"] = false,
+        },
+        on_space_options = { -- A table of vim options when spaces are detected
+            ["expandtab"] = true,
+            ["tabstop"] = "detected", -- If the option value is 'detected', The value is set to the automatically detected indent size.
+            ["softtabstop"] = "detected",
+            ["shiftwidth"] = "detected",
+        },
+    },
+}
